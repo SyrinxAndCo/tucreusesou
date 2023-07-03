6b5a51afa818b6ded137503fc48fa6688f7e0c54
Syrinx <faujoux@gmail.com>
2023-07-03 12:03:19 +0200

    Le Mailer est maintenant opérationnel !

---
+++ src/Helper/Mailer.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace TuCreusesOu\Helper;
+
+use Exception;
+use SendinBlue\Client\Configuration;
+use SendinBlue\Client\Api\TransactionalEmailsApi;
+use GuzzleHttp\Client;
+use SendinBlue\Client\Model\SendSmtpEmail;
+
+class Mailer {
+    static ?Mailer $instance = null;
+    private const ID_TEMPLATE_INSCRIPTION = 2;
+    private TransactionalEmailsApi $api;
+
+    private function __construct() {
+        $credentials = Configuration::getDefaultConfiguration()->setApiKey('api-key', BREVO_MAIL_API_KEY);
+        $this->api = new TransactionalEmailsApi(new Client(), $credentials);
+    }
+
+    /**
+     * Renvoie l'instance du Helper Mailer
+     * @return Mailer
+     */
+    public static function getInstance(): Mailer {
+        if (self::$instance === null) {
+            self::$instance = new Mailer();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Envoie le mail de confirmation d'inscription
+     * @param string $nom
+     * @param string $mail
+     * @param string $code
+     * @return void
+     */
+    public function envoieMailInscription(string $nom, string $mail, string $code): void {
+        $sendSmtpEmail = new SendSmtpEmail();
+        $sendSmtpEmail['to'] = [['name' => $nom, 'email' => $mail]];
+        $sendSmtpEmail['templateId'] = self::ID_TEMPLATE_INSCRIPTION;
+        $sendSmtpEmail['params'] = [
+            'nom' => $nom,
+            'host' => 'https://tucreusesou.localhost/',
+            'code' => $code
+        ];
+
+        try {
+            $this->api->sendTransacEmail($sendSmtpEmail);
+        } catch (Exception $e) {
+            echo $e->getMessage(), PHP_EOL;
+        }
+    }
+}
